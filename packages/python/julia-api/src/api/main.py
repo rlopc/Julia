57d3fccbed6c56c0131dda82d72f317b16819dc5
rlopc4ds <rlopc4ds@gmail.com>
2025-03-02 17:31:44 +0100

    setup for local start and debugging

--- packages/python/julia-api/src/api/main.py
+++ packages/python/julia-api/src/api/main.py
@@ -9,11 +9,15 @@ def check_fastapi() -> dict:
     return {"msg": "'FastAPI' checked!"}
 
 
-if __name__ == "__main__":
+def start_local_server() -> None:
     uvicorn.run(
-        app,
+        app=app,
         host="127.0.0.1",
         port=8000,
         ssl_keyfile="key.pem",
         ssl_certfile="cert.pem",
     )
+
+
+if __name__ == "__main__":
+    start_local_server()
