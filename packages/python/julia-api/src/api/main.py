f1a5e6fab4066d8d4d94a9700ad4e399270d2be6
rlopc4ds <rlopc4ds@gmail.com>
2025-03-02 17:14:59 +0100

    setup fastapi to use https

--- packages/python/julia-api/src/api/main.py
+++ packages/python/julia-api/src/api/main.py
@@ -1,8 +1,19 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def check_fastapi() -> dict:
+def check_fastapi() -> dict:
     return {"msg": "'FastAPI' checked!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="127.0.0.1",
+        port=8000,
+        ssl_keyfile="key.pem",
+        ssl_certfile="cert.pem",
+    )
